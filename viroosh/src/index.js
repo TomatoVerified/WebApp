75bf9142d3104cb2395782c6ad2373903f875e11
TomatoVerified <redstonemasterOP@gmail.com>
2023-05-02 00:18:38 -0400

    init

--- viroosh/src/index.js
+++ viroosh/src/index.js
@@ -11,16 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-/*
-NOTES:
-- index.js and App.js were the main ones
-- functions and classes and stuff we create are referred to as components
-- <App/> is a component 
-- this app thing is getting rendered inside the "root" thing up there ^
-- there is a root div inside the public --> index.html file
-- we are basically injecting the app thing inside the root div in index.html
-- 
-*/
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
